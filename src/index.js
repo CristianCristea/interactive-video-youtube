a4989db2b71bd7c24f83de964606fc55367a66cb
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-10-02 09:05:58 +0200

    Check API response

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/SearchBar";
+import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyC259JkMDG5X4MCBsSZNvDKR8enzANBDCY";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "pokemon" }, videos => {
+      this.setState({ videos });
+      // this.setState({videos: videos})
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 // Render component
 ReactDOM.render(<App />, document.getElementById("root"));

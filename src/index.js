495507c729c95103b90b10ebc48db3cb8cb83668
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-10-02 10:51:29 +0200

    Add event-handlers, render app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 import YTSearch from "youtube-api-search";
 
 const API_KEY = "AIzaSyC259JkMDG5X4MCBsSZNvDKR8enzANBDCY";
@@ -9,18 +12,37 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: "pokemon" }, videos => {
-      this.setState({ videos });
-      // this.setState({videos: videos})
+    this.videoSearch("pokemon");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
+    const { videos, selectedVideo } = this.state;
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={selectedVideo} />
+        <VideoList
+          videos={videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
       </div>
     );
   }
